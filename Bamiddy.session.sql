11d0449f46edcdd74c545cd75bbe7606dc2873e0
bamiddy <bamiddy@gmail.com>
2025-03-19 10:38:56 +0100

    go

--- Bamiddy.session.sql
+++ Bamiddy.session.sql
@@ -1,11 +1,4 @@
--- SELECT * FROM film_category;
-
--- SELECT * FROM "film_actor"
--- LIMIT 10;
-
-/* I Want to update the actor_id of the film_actor
- table to 2 where the film_id is 277 */
-
+SELECT * FROM "actor";
 
 -- SELECT * FROM film_actor
 -- WHERE film_id = 277;
